1e1d1106268c89c075f8025ad2ca88f5bc55ee6d
Oleksand Matviienko <qniwwwersss@gmail.com>
2024-12-13 10:52:07 +0100

    feat: added Dockerfile for Python Health Server

--- server.py
+++ server.py
@@ -11,4 +11,5 @@ def health():
     return jsonify(status="healthy")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # we'd certainly won't use this in production, but for the sake of simplicity, I'd leave it here
+    app.run(host='0.0.0.0')
