9b7e7bda752a244c8c5c44c6a5a2776ee37c043f
Oleksand Matviienko <qniwwwersss@gmail.com>
2024-12-13 12:07:51 +0100

    feat: Implemented AWS Infrastructure setup using CDK (ECR, VPC, SG, Outputs)

--- infra/aws/aws.go
+++ infra/aws/aws.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"github.com/aws/aws-cdk-go/awscdk/v2"
+	cdk "github.com/aws/aws-cdk-go/awscdk/v2"
+	ec2 "github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
+	ecr "github.com/aws/aws-cdk-go/awscdk/v2/awsecr"
 	"os"
 
 	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
@@ -9,27 +11,105 @@ import (
 	"github.com/aws/jsii-runtime-go"
 )
 
+const (
+	// Default CIDR block for the VPC.
+	// This is the IP address range that will be used for the VPC.
+	// As a default, it is class A network.
+	vpcCidrDefault = "10.0.0.0/16"
+
+	// Default mask for the subnets.
+	// This will affect the number of available IP addresses in the subnet.
+	// Recommend you to use it at least /26 for the subnets.
+	// The default value is /24.
+	subnetsMaskDefault = 24
+
+	// Default name for the EKS cluster. This is the name of the EKS cluster that will be created.
+	ecrRepositoryName = "dbbsoftware-test-task-ecr-repository"
+
+	// Default name for	the VPC. This is the name of the VPC that will be created.
+	vpcName = "dbbsoftware-test-task-vpc"
+
+	// createInternetGatewayDefault is a flag that indicates whether the default internet gateway will be created.
+	createInternetGatewayDefault = false
+
+	// enableDnsHostnamesDefault is a flag that indicates whether the DNS hostnames will be enabled.
+	enableDnsHostnamesDefault = true
+
+	// enableDnsSupportDefault is a flag that indicates whether the DNS support will be enabled.
+	enableDnsSupportDefault = true
+)
+
 type AwsStackProps struct {
-	awscdk.StackProps
+	cdk.StackProps
 }
 
-func NewAwsStack(scope constructs.Construct, id string, props *AwsStackProps) awscdk.Stack {
-	var sprops awscdk.StackProps
+func NewAwsStack(scope constructs.Construct, id string, props *AwsStackProps) cdk.Stack {
+	var sprops cdk.StackProps
 	if props != nil {
 		sprops = props.StackProps
 	}
-	stack := awscdk.NewStack(scope, &id, &sprops)
+	stack := cdk.NewStack(scope, &id, &sprops)
+
+	ecrRepo := ecr.NewRepository(stack, jsii.String("DBBSoftwareTestTaskECR"), &ecr.RepositoryProps{
+		EmptyOnDelete:   jsii.Bool(true),
+		ImageScanOnPush: jsii.Bool(true),
+		RepositoryName:  jsii.String(ecrRepositoryName),
+	})
+
+	vpc := ec2.NewVpc(stack, jsii.String("DBBSoftwareTestTaskVPC"), &ec2.VpcProps{
+		CreateInternetGateway: jsii.Bool(createInternetGatewayDefault),
+		EnableDnsHostnames:    jsii.Bool(enableDnsHostnamesDefault),
+		EnableDnsSupport:      jsii.Bool(enableDnsSupportDefault),
+		IpAddresses:           ec2.IpAddresses_Cidr(jsii.String(vpcCidrDefault)),
+		IpProtocol:            ec2.IpProtocol_IPV4_ONLY,
+		MaxAzs:                jsii.Number[int](3),
+		SubnetConfiguration: &[]*ec2.SubnetConfiguration{
+			{
+				Name:                jsii.String("PublicSubnet"),
+				SubnetType:          ec2.SubnetType_PUBLIC,
+				CidrMask:            jsii.Number[int](subnetsMaskDefault),
+				MapPublicIpOnLaunch: jsii.Bool(true),
+			},
+		},
+		VpcName: jsii.String(vpcName),
+	})
+
+	eksSecurityGroup := ec2.NewSecurityGroup(stack, jsii.String("DBBSoftwareTestTaskVpcSg"), &ec2.SecurityGroupProps{
+		Vpc:               vpc,
+		Description:       jsii.String("Allow access to EKS Cluster"),
+		SecurityGroupName: jsii.String("dbb-software-test-task-vpc-sg"),
+	})
+
+	eksSecurityGroup.AddIngressRule(
+		ec2.Peer_AnyIpv4(),
+		ec2.Port_AllTraffic(),
+		jsii.String("Allow all inbound traffic"),
+		jsii.Bool(false),
+	)
+
+	addOutput(stack, "ECR Repository Name", "ECRRepositoryName", ecrRepo.RepositoryName())
+	addOutput(stack, "VPC ID", "VPCID", vpc.VpcId())
+	addOutput(stack, "VPC CIDR", "VPCCIDR", vpc.VpcCidrBlock())
+	addOutput(stack, "Security Group ID", "SecurityGroupID", eksSecurityGroup.SecurityGroupId())
 
 	return stack
+}
+
+func addOutput(stack cdk.Stack, desc string, displayName string, value *string) {
+	cdk.NewCfnOutput(stack, jsii.String(desc), &cdk.CfnOutputProps{
+		ExportName:  jsii.String(displayName),
+		Value:       value,
+		Description: jsii.String(desc),
+	})
 }
 
 func main() {
 	defer jsii.Close()
 
-	app := awscdk.NewApp(nil)
+	app := cdk.NewApp(nil)
 
 	NewAwsStack(app, "DBBSoftware_TestTaskAwsStack", &AwsStackProps{
-		awscdk.StackProps{
+		cdk.StackProps{
 			Env: env(),
 		},
 	})
@@ -39,8 +119,8 @@ func main() {
 
 // env determines the AWS environment (account+region) in which our stack is to
 // be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-func env() *awscdk.Environment {
-	return &awscdk.Environment{
+func env() *cdk.Environment {
+	return &cdk.Environment{
 		Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
 		Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
 	}
